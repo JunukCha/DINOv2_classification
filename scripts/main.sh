56ac0a3409a0fb8938de6ce94b7253658aafce40
unist <jucha@unist.ac.kr>
2023-12-23 13:47:28 +0900

    scripts

---
+++ scripts/main.sh
@@ -0,0 +1,2 @@
+python main.py --backbone_model resnet50
+python main.py --backbone_model dinov2
---
+++ scripts/tsne_interactive.sh
@@ -0,0 +1,2 @@
+python tsne_interactive.py --backbone_model resnet50
+python tsne_interactive.py --backbone_model dinov2
